85541a93ac4042b128892b925504ab64f95d1cda
viskiey <41007676+viskiey@users.noreply.github.com>
2026-02-09 04:26:34 +0300

    Update app.py

--- app.py
+++ app.py
@@ -61,4 +61,4 @@ def update_url():
     })
 
 if __name__ == '__main__':
-    app.run(host='0.0.0.0', port=int(os.environ.get('PORT', 5000)))
+    app.run(host='0.0.0.0', port=int(os.environ.get('PORT', 8080)))
